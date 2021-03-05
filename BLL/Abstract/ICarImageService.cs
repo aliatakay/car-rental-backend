8c340c4096903a0485b7f740a5dfd546ac026d9c
Ali Atakay <aliatakay@gmail.com>
2021-03-05 18:32:48 +0300

    Generated Fundamentals Of CarImage Table

---
+++ BLL/Abstract/ICarImageService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Abstract
+{
+    public interface ICarImageService
+    {
+        IDataResult<CarImage> GetById(int id);
+        IDataResult<List<CarImage>> GetAll();
+        IDataResult<List<CarImage>> GetImagesByCarId(int carId);
+        IResult Add(CarImage carImage);
+        IResult Update(CarImage carImage);
+        IResult Delete(CarImage carImage);
+    }
+}
--- DAL/Concrete/EntityFramework/RentalManagementContext.cs
+++ DAL/Concrete/EntityFramework/RentalManagementContext.cs
@@ -19,6 +19,7 @@ namespace DAL.Concrete.EntityFramework
         public DbSet<User> Users { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Rental> Rentals { get; set; }
+        public DbSet<CarImage> CarImages { get; set; }
 
     }
 }
