b97d15a9467dd9310a7fb0166e69b94160190e20
Ali Atakay <aliatakay@gmail.com>
2021-03-16 23:58:27 +0300

    Configured Cors

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,6 +30,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
             //services.AddSingleton<IBrandService, BrandManager>();
             //services.AddSingleton<IColorService, ColorManager>();
             //services.AddSingleton<IUserService, UserManager>();
@@ -52,6 +53,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
