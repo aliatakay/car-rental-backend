898d1e53e100f3561f5e5a67c4c322687e9ca754
Ali Atakay <aliatakay@gmail.com>
2021-05-14 22:11:45 +0300

    Added Validation Rules for Model

---
+++ BLL/ValidationRules/FluentValidation/ModelValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.ValidationRules.FluentValidation
+{
+    public class ModelValidator : AbstractValidator<Model>
+    {
+        public ModelValidator()
+        {
+            RuleFor(m => m.Name).NotEmpty();
+            RuleFor(m => m.Name).MinimumLength(2);
+            RuleFor(m => m.Name).MaximumLength(200);
+
+            RuleFor(m => m.BrandId).NotEmpty();
+
+            RuleFor(m => m.CategoryId).NotEmpty();
+
+            RuleFor(m => m.Year).NotEmpty();
+            RuleFor(m => m.Year).GreaterThanOrEqualTo(2008);
+            RuleFor(m => m.Year).LessThanOrEqualTo(DateTime.Now.Year);
+        }
+    }
+}
