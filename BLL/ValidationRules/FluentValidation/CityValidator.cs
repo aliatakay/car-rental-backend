1d6fd855e2993c370b539ff76c29f629f2038b60
Ali Atakay <aliatakay@gmail.com>
2021-05-14 22:00:49 +0300

    Added Validation Rules for City

---
+++ BLL/ValidationRules/FluentValidation/CityValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.ValidationRules.FluentValidation
+{
+    public class CityValidator : AbstractValidator<City>
+    {
+        public CityValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MinimumLength(3);
+            RuleFor(c => c.Name).MaximumLength(50);
+        }
+    }
+}
