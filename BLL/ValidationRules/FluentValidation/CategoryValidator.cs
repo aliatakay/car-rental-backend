73683fd8ff16069103c5232a9ca40009aae47b58
Ali Atakay <aliatakay@gmail.com>
2021-05-14 22:07:27 +0300

    Added Validation Rules for Category

---
+++ BLL/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MinimumLength(2);
+            RuleFor(c => c.Name).MaximumLength(200);
+        }
+    }
+}
